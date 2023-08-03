23d12b6762c390e0068d939febb36fbb87c20e5f
Kamali2003 <kamaliseelan03@gmail.com>
2023-08-03 18:19:34 +0530

    jeenu

--- linkforhtml.js
+++ linkforhtml.js
@@ -1,11 +1,11 @@
 document.getElementById("kamali").innerHTML="Hello, Everyone"
 function myfunction1st() {
-    document.getElementById("kamali").innerHTML = "Placement"
+    document.getElementById("hu").innerHTML = "Placement"
 }
 function myfunction2nd(){
-    document.getElementById("kamali").innerHTML ="Higher Studies"
+    document.getElementById("hu").innerHTML ="Higher Studies"
 }
 function myfunction3rd(){
-    document.getElementById("kamali").innerHTML ="Startup"
+    document.getElementById("hu").innerHTML ="Startup"
 }
 
