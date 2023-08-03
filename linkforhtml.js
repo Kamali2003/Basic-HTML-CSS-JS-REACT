b25ab11dd61417ed081e509acb50d0d8bc149e9e
Kamali2003 <kamaliseelan03@gmail.com>
2023-08-03 17:59:53 +0530

    kamali

--- linkforhtml.js
+++ linkforhtml.js
@@ -1,2 +1,11 @@
-document.write("HEllo, Everyone")
-document.getElementById
+document.getElementById("kamali").innerHTML="Hello, Everyone"
+function myfunction1st() {
+    document.getElementById("kamali").innerHTML = "Placement"
+}
+function myfunction2nd(){
+    document.getElementById("kamali").innerHTML ="Higher Studies"
+}
+function myfunction3rd(){
+    document.getElementById("kamali").innerHTML ="Startup"
+}
+
